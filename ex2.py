d7b3f572f0f1a494d9d2a218772397cd5ae46f64
Terry Patterson <10890854+TerryRPatterson@users.noreply.github.com>
2017-12-10 14:17:08 -0500

    Initial commit

---
+++ ex2.py
@@ -0,0 +1,9 @@
+# A comment, this is so you can read the program later.
+#Anything after the # is ingnored by python.
+
+print("I could have a code like this.")# and the comment after is ingnored
+
+#You can also use a comment to "Disable" or comment out code:
+#print("This won't run.")
+
+print("This will run")
---
+++ ex3.py
@@ -0,0 +1,32 @@
+#Prints starting statement
+print("I will now count my chickens:")
+
+#prints count output
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100 - 25 * 3 % 4)
+
+#prints next introductory statement
+print("Now I will count the eggs:")
+
+#prints output
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 -1.0 / 4.0 + 6.0)
+
+#prints user explination
+print("Is it true that 3 + 2 < 5 - 7?")
+
+#prints output
+print(3.0 + 2.0 < 5.0 - 7.0)
+
+#prints user explination and output
+print("What is 3 + 2?", 3.0 + 2.0)
+print("What is 5-7?", 5.0 - 7.0)
+
+#Prints user explination
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+#prints user explination and output
+print("Is it greater?", 5.0 > -2.0)
+print("Is it greater or equal?", 5.0 >= -2.0)
+print("Is it less or equal?", 5.0 <= -2.0)
